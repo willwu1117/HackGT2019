5c6f330320ff7540ce2514ba65ea96e015a58aa6
William Wu <wwu@lawn-128-61-125-207.lawn.gatech.edu>
2019-10-26 03:06:57 -0400

    page changes

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Component, Fragment } from 'react';
+import { Button, View, Text, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator, Platform  } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+import { SearchBar } from 'react-native-elements';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 class HomeScreen extends React.Component {
   render() {
@@ -18,10 +22,95 @@ class HomeScreen extends React.Component {
 }
 
 class SearchScreen extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    //setting default state
+    this.state = { isLoading: true, search: '' };
+    this.arrayholder = [];
+  }
+  componentDidMount() {
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson,
+          },
+          function() {
+            this.arrayholder = responseJson;
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  search = text => {
+    console.log(text);
+  };
+  clear = () => {
+    this.search.clear();
+  };
+  SearchFilterFunction(text) {
+    //passing the inserted text in textinput
+    const newData = this.arrayholder.filter(function(item) {
+      //applying filter for the inserted text in search bar
+      const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      //setting the filtered newData on datasource
+      //After setting the data it will automatically re-render the view
+      dataSource: newData,
+      search:text,
+    });
+  }
+  ListViewItemSeparator = () => {
+    //Item sparator view
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Search</Text>
+      <View
+        style={{
+          height: 0.3,
+          width: '90%',
+          backgroundColor: '#080808',
+        }}
+      />
+    );
+  };
+  render() {
+    if (this.state.isLoading) {
+      //Loading View while data is loading
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+    return (
+      //ListView to show with textinput used as search bar
+      <View style={styles.viewStyle}>
+        <SearchBar
+          round
+          searchIcon={{ size: 24 }}
+          onChangeText={text => this.SearchFilterFunction(text)}
+          onClear={text => this.SearchFilterFunction('')}
+          placeholder="Type Here..."
+          value={this.state.search}
+          />
+          <FlatList
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.ListViewItemSeparator}
+          //Item Separator View
+          renderItem={({ item }) => (
+            // Single Comes here which will be repeatative for the FlatListItems
+            <Text style={styles.textStyle}>{item.title}</Text>
+          )}
+          enableEmptySections={true}
+          style={{ marginTop: 10 }}
+          keyExtractor={(item, index) => index.toString()}
+        />
         <Button
           title="Begin Movie"
           onPress={() => this.props.navigation.navigate('Timer')}
@@ -32,14 +121,120 @@ class SearchScreen extends React.Component {
 }
 
 class TimerScreen extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      timer: null,
+      minutes_Counter: '00',
+      seconds_Counter: '00',
+      startDisable: false
+    }
+  }
+ 
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+ 
+  onButtonStart = () => {
+    let timer = setInterval(() => {
+      var num = (Number(this.state.seconds_Counter) + 1).toString(),
+        count = this.state.minutes_Counter;
+ 
+      if (Number(this.state.seconds_Counter) == 59) {
+        count = (Number(this.state.minutes_Counter) + 1).toString();
+        num = '00';
+      }
+ 
+      this.setState({
+        minutes_Counter: count.length == 1 ? '0' + count : count,
+        seconds_Counter: num.length == 1 ? '0' + num : num
+      });
+    }, 1000);
+    this.setState({ timer });
+ 
+    this.setState({startDisable : true})
+  }
+ 
+  onButtonStop = () => {
+    clearInterval(this.state.timer);
+    this.setState({startDisable : false})
+  }
+ 
+  onButtonClear = () => {
+    this.setState({
+      timer: null,
+      minutes_Counter: '00',
+      seconds_Counter: '00',
+    });
+  }
+ 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Timer</Text>
+      <View style={styles.MainContainer}>
+ 
+        <Text style={styles.counterText}>{this.state.minutes_Counter} : {this.state.seconds_Counter}</Text>
+ 
+        <TouchableOpacity
+          onPress={this.onButtonStart}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor: this.state.startDisable ? '#B0BEC5' : '#FF6F00' }]} 
+          disabled={this.state.startDisable} >
+          <Text style={styles.buttonText}>START</Text>
+        </TouchableOpacity>
+ 
+        <TouchableOpacity
+          onPress={this.onButtonStop}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor:  '#FF6F00'}]} >
+          <Text style={styles.buttonText}>STOP</Text>
+        </TouchableOpacity>
+ 
+        <TouchableOpacity
+          onPress={this.onButtonClear}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor: this.state.startDisable ? '#B0BEC5' : '#FF6F00' }]} 
+          disabled={this.state.startDisable} >
+          <Text style={styles.buttonText}> CLEAR </Text>
+        </TouchableOpacity>
       </View>
     );
   }
-}
+} 
+ 
+const styles = StyleSheet.create({
+  viewStyle: {
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor:'white',
+    marginTop: Platform.OS == 'ios'? 30 : 0
+  },
+  textStyle: {
+    padding: 10,
+  },
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    width: '80%',
+    paddingTop:8,
+    paddingBottom:8,
+    borderRadius:7,
+    marginTop: 10
+  },
+  buttonText:{
+      color:'#fff',
+      textAlign:'center',
+      fontSize: 20
+  },
+  counterText:{
+    fontSize: 28,
+    color: '#000'
+  }
+});
 
 const RootStack = createStackNavigator(
   {
