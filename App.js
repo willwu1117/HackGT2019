78e55b76e135c8b807c189011c02425f904ab0e9
Edward Xia <37724514+EdwardXia3@users.noreply.github.com>
2019-10-26 13:54:12 -0400

    Update

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
+
 import * as React from 'react';
-import { Button, View, Text, StyleSheet, Image} from 'react-native';
+import { Component, Fragment } from 'react';
+import { Button, View, Text, TouchableOpacity, StyleSheet, FlatList, ActivityIndicator, Platform, Image  } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+import { SearchBar, CheckBox } from 'react-native-elements';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -16,7 +21,6 @@ class HomeScreen extends React.Component {
        backgroundColor: 'black'}}>
         <Text style={styles.white}>ADVERSCARY!</Text>
         <Image source={pic} style={{width: 250, height: 250}}/>
-
         <Button
           title="Get Started"
           color='#000000'
@@ -28,11 +32,63 @@ class HomeScreen extends React.Component {
   }
 }
 
+const movies = require('./movie.json');
+
 class SearchScreen extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <View>
+        <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+        <Button
+          title="What would you like to avoid?"
+          onPress={() => this.props.navigation.navigate('CheckList')}
+        />
+      </View>
+    );
+  }
+}
+
+class CheckListScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jumpscares: false,
+      gore: false
+    }
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Search</Text>
+      <View style={styles.container}>
+        <Text>Select what you would like to be warned about.</Text>
+
+        <CheckBox
+          title='Jump scares'
+          checked={this.state.jumpscares}
+          onPress={() => this.setState({jumpscares: !this.state.jumpscares})}
+        />
+
+        <CheckBox
+          title='Gore'
+          checked={this.state.gore}
+          onPress={() => this.setState({gore: !this.state.gore})}
+        />
+
         <Button
           title="Begin Movie"
           onPress={() => this.props.navigation.navigate('Timer')}
@@ -43,10 +99,82 @@ class SearchScreen extends React.Component {
 }
 
 class TimerScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timer: null,
+      minutes_Counter: '00',
+      seconds_Counter: '00',
+      startDisable: false
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+
+  onButtonStart = () => {
+    let timer = setInterval(() => {
+      var num = (Number(this.state.seconds_Counter) + 1).toString(),
+        count = this.state.minutes_Counter;
+
+      if (Number(this.state.seconds_Counter) == 59) {
+        count = (Number(this.state.minutes_Counter) + 1).toString();
+        num = '00';
+      }
+
+      this.setState({
+        minutes_Counter: count.length == 1 ? '0' + count : count,
+        seconds_Counter: num.length == 1 ? '0' + num : num
+      });
+    }, 1000);
+    this.setState({ timer });
+
+    this.setState({startDisable : true})
+  }
+
+  onButtonStop = () => {
+    clearInterval(this.state.timer);
+    this.setState({startDisable : false})
+  }
+
+  onButtonClear = () => {
+    this.setState({
+      timer: null,
+      minutes_Counter: '00',
+      seconds_Counter: '00',
+    });
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Timer</Text>
+      <View style={styles.MainContainer}>
+
+        <Text style={styles.counterText}>{this.state.minutes_Counter} : {this.state.seconds_Counter}</Text>
+
+        <TouchableOpacity
+          onPress={this.onButtonStart}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor: this.state.startDisable ? '#B0BEC5' : '#FF6F00' }]}
+          disabled={this.state.startDisable} >
+          <Text style={styles.buttonText}>START</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={this.onButtonStop}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor:  '#FF6F00'}]} >
+          <Text style={styles.buttonText}>STOP</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={this.onButtonClear}
+          activeOpacity={0.6}
+          style={[styles.button, { backgroundColor: this.state.startDisable ? '#B0BEC5' : '#FF6F00' }]}
+          disabled={this.state.startDisable} >
+          <Text style={styles.buttonText}> CLEAR </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -78,6 +206,28 @@ const styles = StyleSheet.create({
   },
   black: {
     color:'black'
+  },
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    width: '80%',
+    paddingTop:8,
+    paddingBottom:8,
+    borderRadius:7,
+    marginTop: 10
+  },
+  buttonText:{
+      color:'#fff',
+      textAlign:'center',
+      fontSize: 20
+  },
+  counterText:{
+    fontSize: 28,
+    color: '#000'
   }
 });
 
